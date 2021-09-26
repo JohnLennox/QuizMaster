5b32a4ac0fffa800b03990260d4a03b80a267329
John Lennox <jl1998@hotmail.co.uk>
2021-09-26 21:13:56 +0100

    Added findById endpoint for Quiz
    Added a Custom QuizNotFoundException
    
    Created an Exception Handler and added a method that deals with not found errors

--- src/main/java/com/quizproject/quizmaster/controllers/QuizController.java
+++ src/main/java/com/quizproject/quizmaster/controllers/QuizController.java
@@ -4,8 +4,12 @@ import com.quizproject.quizmaster.dto.QuizDTO;
 import com.quizproject.quizmaster.dto.mappers.QuizDtoMapper;
 import com.quizproject.quizmaster.dto.mappers.QuizDtoMapperImpl;
 import com.quizproject.quizmaster.entities.Quiz;
+import com.quizproject.quizmaster.exception.QuizNotFoundException;
 import com.quizproject.quizmaster.repositories.QuizRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -15,7 +19,7 @@ import java.util.List;
 public class QuizController {
     public static final String BASE = "/quiz";
     private final QuizRepository quizRepository;
-    private QuizDtoMapper mapper;
+    private final QuizDtoMapper mapper;
 
     public QuizController(QuizRepository quizRepository) {
         this.quizRepository = quizRepository;
@@ -23,11 +27,20 @@ public class QuizController {
     }
 
     @GetMapping(BASE + "/all")
-    List<QuizDTO> getAll(){
+    public List<QuizDTO> getAll(){
         var quizzes = new ArrayList<QuizDTO>();
         for(Quiz quiz : quizRepository.findAll()){
             quizzes.add(mapper.quizToDTO(quiz));
         }
         return quizzes;
+    }
+
+    @GetMapping(BASE + "/findById/{id}")
+    public ResponseEntity<QuizDTO> findById(@PathVariable Long id){
+        var quiz = quizRepository.findById(id);
+        if (quiz.isPresent()) {
+            return new ResponseEntity<>(mapper.quizToDTO(quiz.get()), HttpStatus.OK);
+        }
+        throw new QuizNotFoundException(HttpStatus.NOT_FOUND, "Quiz with ID: " +id + " not found", null);
     }
 }
---
+++ src/main/java/com/quizproject/quizmaster/exception/QuizNotFoundException.java
@@ -0,0 +1,21 @@
+package com.quizproject.quizmaster.exception;
+
+import org.springframework.core.NestedRuntimeException;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
+
+public class QuizNotFoundException extends NestedRuntimeException {
+
+    private final int status;
+
+    @Nullable
+    private final String reason;
+
+    public QuizNotFoundException(HttpStatus status, @Nullable String reason, @Nullable Throwable cause) {
+        super((String)null, cause);
+        Assert.notNull(status, "HttpStatus is required");
+        this.status = status.value();
+        this.reason = reason;
+    }
+}
